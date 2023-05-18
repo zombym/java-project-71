208451ae5ebc2dcc1db8a0d2caea1765dc37b31a
Aleksandr Ponomarev <aponomarev@smartyco.ru>
2023-05-18 21:34:53 +0400

    prj2 task4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,24 @@
 package hexlet.code;
 
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
 
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1",
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff: 1",
         description = "Compares two configuration files and shows a difference.")
 
 public class App implements Runnable{
+
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String firstFilePath;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String secondFilePath;
+
+    @CommandLine.Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
+    private boolean usageHelpRequested;
+    @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionInfoRequested;
+    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
     public static void main(String[] args) {
         new CommandLine(new App()).execute(args);
     }
