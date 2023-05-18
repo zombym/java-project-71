aeeb8a08b56c3e5142677d34274101d00f2599c5
Aleksandr Ponomarev <aponomarev@smartyco.ru>
2023-05-18 21:17:27 +0400

    prj2 task3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1",
+        description = "Compares two configuration files and shows a difference.")
+
+public class App implements Runnable{
     public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
 }
